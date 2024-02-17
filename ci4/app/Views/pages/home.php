2cb48179bb914355cf90899f478e250d34e5c99a
aoditano <aoditano@student.apc.edu.ph>
2024-02-17 16:03:58 +0800

    update

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -1,1 +1,489 @@
-Hello im hash!
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+    />
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic&amp;display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=ABeeZee&amp;display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Abhaya+Libre&amp;display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Advent+Pro&amp;display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Albert+Sans&amp;display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Alfa+Slab+One&amp;display=swap"
+    />
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css" />
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css" />
+    <link rel="stylesheet" href="assets/css/Banner-Heading-Image-images.css" />
+  </head>
+
+  <body id="page-top">
+    <nav class="navbar navbar-expand navbar-light" id="sidebar-wrapper">
+      <div class="container">
+        <button
+          data-bs-toggle="collapse"
+          class="navbar-toggler d-none"
+          data-bs-target="#"
+        ></button>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav sidebar-nav" id="sidebar-nav">
+            <li class="nav-item sidebar-brand">
+              <a class="nav-link active js-scroll-trigger" href="#page-top"
+                >Brand</a
+              >
+            </li>
+            <li class="nav-item sidebar-nav-item">
+              <a class="nav-link js-scroll-trigger" href="#page-top">Home</a>
+            </li>
+            <li class="nav-item sidebar-nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item sidebar-nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services"
+                >Services</a
+              >
+            </li>
+            <li class="nav-item sidebar-nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio"
+                >Portfolio</a
+              >
+            </li>
+            <li class="nav-item sidebar-nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <header
+      class="d-flex masthead"
+      style="background: url('assets/img/Azure%20Pop.jpg')"
+    >
+      <div class="container my-auto text-center">
+        <h1
+          class="mb-1"
+          style="
+            color: var(--bs-body-bg);
+            font-size: 95px;
+            text-align: center;
+            font-family: 'Source Sans Pro', sans-serif;
+            padding-bottom: 15px;
+            text-shadow: 5px 0px 2px var(--bs-pink);
+          "
+        >
+          Aloha! Welcome to my personal website
+        </h1>
+        <h3 class="mb-5">
+          <em
+            style="
+              color: var(--bs-body-bg);
+              font-size: 30px;
+              text-shadow: 0px 0px;
+            "
+            >Enjoy!</em
+          >
+        </h3>
+        <a
+          class="btn btn-primary btn-xl js-scroll-trigger"
+          role="button"
+          href="#about"
+          style="
+            color: var(--bs-btn-active-color);
+            background: linear-gradient(99deg, #b2abee, #e83fdb 100%), #ff43bf;
+            border-radius: 110px;
+            border-style: solid;
+            border-color: var(--bs-btn-active-color);
+          "
+          >Find Out More About Hash</a
+        >
+        <div class="overlay"></div>
+      </div>
+      <div class="carousel slide" data-bs-ride="false" id="carousel-1">
+        <div class="carousel-inner">
+          <div class="carousel-item active"></div>
+          <div class="carousel-item">
+            <img
+              class="w-100 d-block"
+              src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"
+              alt="Slide Image"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              class="w-100 d-block"
+              src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"
+              alt="Slide Image"
+            />
+          </div>
+        </div>
+        <div>
+          <a
+            class="carousel-control-prev"
+            href="#carousel-1"
+            role="button"
+            data-bs-slide="prev"
+            ><span class="carousel-control-prev-icon"></span
+            ><span class="visually-hidden">Previous</span></a
+          ><a
+            class="carousel-control-next"
+            href="#carousel-1"
+            role="button"
+            data-bs-slide="next"
+            ><span class="carousel-control-next-icon"></span
+            ><span class="visually-hidden">Next</span></a
+          >
+        </div>
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carousel-1"
+            data-bs-slide-to="0"
+            class="active"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carousel-1"
+            data-bs-slide-to="1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carousel-1"
+            data-bs-slide-to="2"
+          ></button>
+        </div>
+      </div>
+    </header>
+    <section id="about" class="content-section bg-light">
+      <div></div>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h2 style="color: rgb(236, 56, 218)">
+              A short description of myself
+            </h2>
+            <p class="lead mb-5">
+              <span
+                ><br /><br /><span style="color: rgb(55, 65, 81)"
+                  >I am Abdul Hasheem Omega Ditano, though you may refer to me
+                  as Hash. I am 19 years old, born on February 13. Currently, I
+                  am a student pursuing a degree in Mobile and Internet
+                  Technology within the field of Information Technology at Asia
+                  Pacific College.</span
+                ></span
+              ><a href="https://unsplash.com/"></a>
+            </p>
+            <a
+              class="btn btn-dark btn-xl js-scroll-trigger"
+              role="button"
+              href="#services"
+              style="
+                background: #ffffff;
+                border-style: solid;
+                border-color: #ff43bf;
+                border-radius: 110px;
+                color: #de5ac2;
+              "
+              >Go to My Facts</a
+            >
+          </div>
+          <div class="col" style="display: flex; border-radius: 15px">
+            <picture
+              ><img
+                src="assets/img/416980605_389182816933183_8866666184519171758_n%20(1).jpg"
+                style="
+                  display: flex;
+                  justify-content: right;
+                  align-items: start;
+                  height: 45vh;
+                  transform: translate(24px);
+                  border-radius: 15px;
+                  border-top-right-radius: 15px;
+                  border-bottom-left-radius: 15px;
+                  border: 4.5px solid rgb(176, 176, 239);
+                  box-shadow: 9.5px 9.5px 0px #b0b0ef;
+                "
+            /></picture>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section
+      id="services"
+      class="content-section bg-primary text-white text-center"
+      style="background: url('assets/img/Azure%20Pop.jpg')"
+    >
+      <div class="container">
+        <div class="content-section-heading">
+          <h3 class="text-secondary mb-0">Hash</h3>
+          <h2 class="mb-5">Facts About Me</h2>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+            <span class="mx-auto service-icon rounded-circle mb-3"
+              ><i class="icon-social-instagram"></i
+            ></span>
+            <h4><strong>Social Media</strong></h4>
+            <p class="mb-0 text-faded">
+              <span style="color: rgb(254, 255, 255)"
+                >I love social media; it's one of my stress relievers</span
+              >
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+            <span class="mx-auto service-icon rounded-circle mb-3"
+              ><i class="icon-badge"></i
+            ></span>
+            <h4><strong>Achievement</strong></h4>
+            <p class="mb-0 text-faded">
+              <span style="color: rgb(251, 251, 251)"
+                >I graduated with high honors from Taguig National High School
+                with a specialization in the HUMSS strand.</span
+              >
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+            <span class="mx-auto service-icon rounded-circle mb-3"
+              ><i class="icon-heart"></i
+            ></span>
+            <h4><strong>My Religion</strong></h4>
+            <p class="mb-0 text-faded">
+              <span
+                ><span style="color: rgb(255, 255, 255)"
+                  >I am a Muslim, and I deeply appreciate and value my
+                  non-Muslim friends&nbsp;</span
+                ></span
+              ><i class="fa fa-heart"></i><span></span>
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+            <span class="mx-auto service-icon rounded-circle mb-3"
+              ><i class="icon-people"></i
+            ></span>
+            <h4><strong>Personality</strong></h4>
+            <p class="mb-0 text-faded" style="color: var(--bs-body-bg)">
+              <span style="color: rgb(242, 242, 242)"
+                >I am an extrovert; I always want to be surrounded by my family
+                and friends.</span
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section
+      class="callout"
+      style="
+        background: linear-gradient(
+              90deg,
+              rgba(255, 255, 255, 0.1) 0%,
+              rgba(255, 255, 255, 0.1) 100%
+            )
+            center center / cover no-repeat,
+          url('assets/img/Azure%20Pop.jpg');
+      "
+    >
+      <div class="container text-center">
+        <h2 class="mx-auto mb-5" style="color: var(--bs-body-bg)">
+          <span>Welcome to&nbsp;</span><em>the</em><span><br />website!</span>
+        </h2>
+        <a
+          class="btn btn-primary btn-xl"
+          role="button"
+          href="#"
+          style="
+            background: rgb(231, 91, 183);
+            border: 3px solid rgb(249, 221, 221);
+          "
+          >XXXXXXXXXX</a
+        >
+      </div>
+    </section>
+    <section id="portfolio" class="content-section">
+      <div class="container">
+        <div class="content-section-heading text-center">
+          <h3 class="text-secondary mb-0">Portfolio</h3>
+          <h2 class="mb-5" style="color: rgb(222, 90, 194)">My Life</h2>
+        </div>
+        <div class="row g-0">
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <div
+                class="caption"
+                style="background: url('assets/img/Untitled%20design.jpg')"
+              >
+                <div class="caption-content">
+                  <h2>XXXXXXXXXXXXXX</h2>
+                  <p class="mb-0">XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+                </div>
+              </div>
+              <img class="img-fluid" src="assets/img/portfolio-1.jpg" />
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <div
+                class="caption"
+                style="
+                  background: url('assets/img/F339C5E0-B428-401C-A739-84A7A0DE5469.jpg');
+                "
+              >
+                <div class="caption-content">
+                  <h2>XXXXXXXXXXXXXXXXXX</h2>
+                  <p class="mb-0">XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+                </div>
+              </div>
+              <img class="img-fluid" src="assets/img/portfolio-2.jpg" />
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <div
+                class="caption"
+                style="
+                  background: url('assets/img/821D2F2E-8DA1-4935-BB2E-665B1CD1ABB7.jpg');
+                "
+              >
+                <div class="caption-content">
+                  <h2>XXXXXXXXXXXXXXX</h2>
+                  <p class="mb-0">XXXXXXXXXXXXXXXXX</p>
+                </div>
+              </div>
+              <img class="img-fluid" src="assets/img/portfolio-3.jpg" />
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <div
+                class="caption"
+                style="
+                  background: url('assets/img/7B153AA7-68D7-4E37-9D3B-FA92DEA165CB.jpg');
+                "
+              >
+                <div class="caption-content">
+                  <h2>XXXXXXXXXXXXXXX</h2>
+                  <p class="mb-0">XXXXXXXXXXXXXXXXXXX</p>
+                </div>
+              </div>
+              <img class="img-fluid" src="assets/img/portfolio-4.jpg" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="footer text-center">
+      <ul class="list-inline mb-5">
+        <li class="list-inline-item">
+          &nbsp;<a
+            class="link-light social-link rounded-circle"
+            href="#"
+            style="background: #fe37ea"
+            ><i class="icon-social-facebook"></i
+          ></a>
+        </li>
+        <li class="list-inline-item">
+          &nbsp;<a
+            class="link-light social-link rounded-circle"
+            href="#"
+            style="background: #fe37ea"
+            ><i class="icon-social-twitter"></i
+          ></a>
+        </li>
+        <li class="list-inline-item">
+          &nbsp;<a
+            class="link-light social-link rounded-circle"
+            href="#"
+            style="background: rgb(254, 55, 234)"
+            ><i class="icon-social-instagram"></i
+          ></a>
+        </li>
+      </ul>
+      <div class="container">
+        <p class="text-muted mb-0 small">Copyright &nbsp;© Brand 2024</p>
+      </div>
+      <a class="js-scroll-trigger scroll-to-top rounded" href="#page-top"
+        ><i class="fa fa-angle-up"></i
+      ></a>
+
+<footer class="footer text-center">
+  <ul class="list-inline mb-5">
+ 
+  <div class="container">
+    <p class="text-muted mb-0 small">Copyright &nbsp;© Brand 2024</p>
+  </div>
+  <a class="js-scroll-trigger scroll-to-top rounded" href="#page-top">
+    <i class="fa fa-angle-up"></i>
+  </a>
+  <script type="module">
+    import message from "./message.js";
+
+    const person = {
+      firstName: "Hasheem",
+      lastName: "Ditano",
+      age: 19,
+      haircolor: "blue"
+    };
+
+    document.getElementById("demo").innerHTML =
+      person.firstName + " is " + person.age + " years old.";
+
+    let text1 = "Outside: " + typeof person.firstName;
+    document.getElementById("demo1").innerHTML = text1;
+
+    function myFunction() {
+      let carName = "Porsche";
+      let text2 = "Inside: " + typeof carName + " " + carName;
+      document.getElementById("demo2").innerHTML = text2;
+    }
+
+    myFunction();
+
+    class Car {
+      constructor(name, year) {
+        this.name = name;
+        this.year = year;
+      }
+    }
+
+    const myCar1 = new Car("Ford", 2014);
+    const myCar2 = new Car("Audi", 2019);
+
+    function myDisplayer(some) {
+      document.getElementById("demo").innerHTML = some;
+    }
+
+    function myFirst() {
+      myDisplayer("Aloha");
+    }
+
+    function mySecond() {
+      myDisplayer("Aloha");
+    }
+
+    myFirst();
+    mySecond();
+  </script>
+</footer>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/stylish-portfolio.js"></script>
+</body>
+</html>
